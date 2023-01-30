d683d643c27968aa354b4752cadef0e9f9cc403b
Muhammet İNCE <83353864+MuhammetInceee@users.noreply.github.com>
2023-01-30 11:29:21 +0300

    Uploaded

---
+++ Assets/Scripts/Controllers/Player/PlayerMovementController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Data.ValueObjects;
+using DG.Tweening;
+using Keys;
+using Managers;
+using Signals;
+using Sirenix.OdinInspector;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Controllers.Player
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private PlayerManager manager;
+
+        [SerializeField] private new Rigidbody rigidbody;
+        [SerializeField] private new Collider collider;
+
+        #endregion
+
+        #region Private Variables
+
+        [ShowInInspector] private MovementData _data;
+
+        [ShowInInspector] private bool _isReadyToMove, _isReadyToPlay;
+
+        private float _xValue;
+        private float2 _clampValues;
+        private float _tempSpeed;
+        
+        #endregion
+
+        #endregion
+
+        #region Properties
+
+        #region Public Properties
+
+        public Vector3 VelocityVector => rigidbody.velocity;
+
+        #endregion
+
+        #endregion
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            CoreGameSignals.Instance.onLevelEnd += OnLevelEnd;
+        }
+
+        private void UnSubscribeEvents()
+        {
+            CoreGameSignals.Instance.onLevelEnd -= OnLevelEnd;
+        }
+
+        private void OnLevelEnd()
+        {
+            _tempSpeed = _data.ForwardSpeed * _data.MiniGameMultiplier;
+            DOTween.To(() => _tempSpeed, x => _tempSpeed = x, 5, UnityEngine.Random.Range(3, 7))
+                .OnUpdate(() =>
+                {
+                    _data.ForwardSpeed = _tempSpeed;
+                })
+                .OnComplete(() =>
+                {
+                    _data.ForwardSpeed = 0;
+                    DOVirtual.DelayedCall(3, () =>
+                    {
+                        CoreGameSignals.Instance.onLevelSuccessful?.Invoke();
+                    });
+                });
+        }
+
+        internal void SetMovementData(MovementData movementData)
+        {
+            _data = movementData;
+        }
+
+        private void FixedUpdate()
+        {
+            if (!_isReadyToPlay)
+            {
+                StopPlayer();
+                return;
+            }
+
+            if (_isReadyToMove)
+            {
+                MovePlayer();
+            }
+            else
+            {
+                StopPlayerHorizontally();
+            }
+        }
+
+        private void MovePlayer()
+        {
+            var velocity = rigidbody.velocity;
+            velocity = new float3(_xValue * _data.SidewaysSpeed, velocity.y,
+                _data.ForwardSpeed);
+            rigidbody.velocity = velocity;
+
+            float3 position;
+            position = new float3(
+                Mathf.Clamp(rigidbody.position.x, _clampValues.x,
+                    _clampValues.y),
+                (position = rigidbody.position).y,
+                position.z);
+            rigidbody.position = position;
+        }
+
+        private void StopPlayerHorizontally()
+        {
+            rigidbody.velocity = new float3(0, rigidbody.velocity.y, _data.ForwardSpeed);
+            rigidbody.angularVelocity = float3.zero;
+        }
+
+        private void StopPlayer()
+        {
+            rigidbody.velocity = float3.zero;
+            rigidbody.angularVelocity = float3.zero;
+        }
+
+        internal void IsReadyToPlay(bool condition)
+        {
+            _isReadyToPlay = condition;
+        }
+
+        internal void IsReadyToMove(bool condition)
+        {
+            _isReadyToMove = condition;
+        }
+
+        internal void UpdateInputParams(HorizontalnputParams inputParams)
+        {
+            _xValue = inputParams.HorizontalInputValue;
+            _clampValues = new float2(inputParams.HorizontalInputClampNegativeSide,
+                inputParams.HorizontalInputClampPositiveSide);
+        }
+
+        internal void OnReset()
+        {
+            StopPlayer();
+            _isReadyToPlay = false;
+            _isReadyToMove = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/Player/PlayerPhysicsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Collidables;
+using Controllers.Pool;
+using DG.Tweening;
+using Managers;
+using Signals;
+using UnityEngine;
+
+namespace Controllers.Player
+{
+    public class PlayerPhysicsController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private PlayerManager manager;
+        [SerializeField] private PlayerMovementController movementController;
+        [SerializeField] private new Collider collider;
+        [SerializeField] private new Rigidbody rigidbody;
+
+        #endregion
+
+        #region Private Variables
+        
+        #endregion
+
+        #endregion
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("StageArea"))
+            {
+                manager.ForceCommand.Execute();
+                CoreGameSignals.Instance.onStageAreaEntered?.Invoke();
+                InputSignals.Instance.onDisableInput?.Invoke();
+
+                DOVirtual.DelayedCall(3, () =>
+                {
+                    // if (other.TryGetComponent(out PoolController pool))
+                    // {
+                    //     bool resul = pool.TakeStageResult(manager.StageID);
+                    //     _lastCollectedAmount = pool.TakeCollectedAmount();
+                    //
+                    //     if (resul)
+                    //     {
+                    //         CoreGameSignals.Instance.onStageAreaSuccessful?.Invoke(manager.StageID);
+                    //         UISignals.Instance.onSetStageColor?.Invoke(manager.StageID);
+                    //         InputSignals.Instance.onEnableInput?.Invoke();
+                    //         manager.StageID++;
+                    //     }
+                    //     else
+                    //     {
+                    //         CoreGameSignals.Instance.onLevelFailed?.Invoke();
+                    //     }
+                    // }
+                    var result = other.transform.parent.GetComponentInChildren<PoolController>()
+                        .TakeStageResult(manager.StageID);
+                    if (result)
+                    {
+                        CoreGameSignals.Instance.onStageAreaSuccessful?.Invoke(manager.StageID);
+                        UISignals.Instance.onSetStageColor?.Invoke(manager.StageID);
+                        InputSignals.Instance.onEnableInput?.Invoke();
+                        manager.StageID++;
+                    }
+                    else CoreGameSignals.Instance.onLevelFailed?.Invoke();
+                });
+                return;
+            }
+
+            if (other.CompareTag("Finish"))
+            {
+                CoreGameSignals.Instance.onLevelEnd?.Invoke();
+            }
+        }
+
+        private string _message;
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.TryGetComponent(out DiamondBox diamondBox))
+            {
+                if(movementController.VelocityVector.z > 0) return;
+                if(manager.LevelEndDiamond == diamondBox.value) return;
+
+                _message = "+ " + manager.LevelEndDiamond;
+                manager.LevelEndDiamond = diamondBox.value;
+                
+                MessageManager.instance.Show(_message);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            var transform1 = manager.transform;
+            var position = transform1.position;
+            Gizmos.DrawSphere(new Vector3(position.x, position.y - 1.2f, position.z + 1f), 1.65f);
+        }
+
+        public void OnReset()
+        {
+        }
+    }
+}
